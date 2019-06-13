ee3f914fc9d128ce43cbd22d1f42421e413e3ef9
fackyhigh <50703972+fackyhigh@users.noreply.github.com>
2019-06-13 17:27:58 +0300

    Added more basic styles for items

--- prices.php
+++ prices.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link href="css/external.css" rel="stylesheet">
     <title>Document</title>
@@ -22,37 +23,37 @@
             </div>
             <div class="item-description">
                 <p>ПАКЕТ ВКЛЮЧАЕТ В СЕБЯ:</p>
-                <p>30-40 минут съемки.</p>
+                <p>30-40 минут съемки</p>
                 <p>5 фотографий в ретуши</p>
-                <p>Все остальные хорошие фотографии в базовой цветокоррекции </p>
-                <p>Фотографии передаются по интернету.</p>
+                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
+                <p>Фотографии передаются по интернету</p>
             </div>
         </div>
         <div class="item">
             <div class="item-header">
-                <h3></h3>
+                <h3>Пакет "Стандартный"</h3>
                 <span class="header-divider"></span>
-                <p class="price"></p>
+                <p class="price">10 000 руб.</p>
             </div>
             <div class="item-description">
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
+                <p>1-1,5 часа съемки.</p>
+                <p>15 фотографий в ретуши</p>
+                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
             </div>
         </div>
         <div class="item">
             <div class="item-header">
-                <h3></h3>
+                <h3>Пакет "Люкс"</h3>
                 <span class="header-divider"></span>
-                <p class="price"></p>
+                <p class="price">15 000 руб.</p>
             </div>
             <div class="item-description">
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
+                <p>1,5-2 часа съемки</p>
+                <p>25 фотографий в ретуши</p>
+                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
+                <p>Минибук-фотокнига 20*20 см (10 разворотов) с результатами съемки</p>
             </div>
         </div>
     </div>
