7e0a59b6a2a0cdf9ef15bb04e25cf939b13d0a96
fackyhigh <50703972+fackyhigh@users.noreply.github.com>
2019-06-14 11:46:11 +0300

    ... more styles

--- prices.php
+++ prices.php
@@ -11,51 +11,61 @@
 </head>
 <body>
     <?php include "menu.php" ?>
-    <h1 class="title">Услуги и цены</h1>
-    <span class="title-divider"></span>
+    
     <div class="content">
-        <h2>Фотопрогулка</h2>
-        <div class="item">
-            <div class="item-header">
-                <h3>Пакет "Минимальный"</h3>
-                <span class="header-divider"></span>
-                <p class="price">7 000 руб.</p>                
-            </div>
-            <div class="item-description">
-                <p>ПАКЕТ ВКЛЮЧАЕТ В СЕБЯ:</p>
-                <p>30-40 минут съемки</p>
-                <p>5 фотографий в ретуши</p>
-                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
-                <p>Фотографии передаются по интернету</p>
-            </div>
+        
+        <div class="header-wrapper">
+            <h1 class="title">Услуги и цены</h1>
+            <span class="title-divider"></span>
+            
         </div>
-        <div class="item">
-            <div class="item-header">
-                <h3>Пакет "Стандартный"</h3>
-                <span class="header-divider"></span>
-                <p class="price">10 000 руб.</p>
-            </div>
-            <div class="item-description">
-                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
-                <p>1-1,5 часа съемки.</p>
-                <p>15 фотографий в ретуши</p>
-                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
-            </div>
+        <div class="content-center">
+                <div class="item-wrapper">
+                        <h2>Фотопрогулка</h2>
+                        <div class="item">
+                            <div class="item-header">
+                                <h3>Пакет "Минимальный"</h3>
+                                <span class="header-divider"></span>
+                                <p class="price">7 000 руб.</p>                
+                            </div>
+                            <div class="item-description">
+                                <p>ПАКЕТ ВКЛЮЧАЕТ В СЕБЯ:</p>
+                                <p>30-40 минут съемки</p>
+                                <p>5 фотографий в ретуши</p>
+                                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
+                                <p>Фотографии передаются по интернету</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item-header">
+                                <h3>Пакет "Стандартный"</h3>
+                                <span class="header-divider"></span>
+                                <p class="price">10 000 руб.</p>
+                            </div>
+                            <div class="item-description">
+                                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
+                                <p>1-1,5 часа съемки.</p>
+                                <p>15 фотографий в ретуши</p>
+                                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item-header">
+                                <h3>Пакет "Люкс"</h3>
+                                <span class="header-divider"></span>
+                                <p class="price">15 000 руб.</p>
+                            </div>
+                            <div class="item-description">
+                                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
+                                <p>1,5-2 часа съемки</p>
+                                <p>25 фотографий в ретуши</p>
+                                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
+                                <p>Минибук-фотокнига 20*20 см (10 разворотов) с результатами съемки</p>
+                            </div>
+                        </div>
+                </div> 
         </div>
-        <div class="item">
-            <div class="item-header">
-                <h3>Пакет "Люкс"</h3>
-                <span class="header-divider"></span>
-                <p class="price">15 000 руб.</p>
-            </div>
-            <div class="item-description">
-                <p>ТАРИФ ВКЛЮЧАЕТ В СЕБЯ:</p>
-                <p>1,5-2 часа съемки</p>
-                <p>25 фотографий в ретуши</p>
-                <p>Все остальные хорошие фотографии в базовой цветокоррекции</p>
-                <p>Минибук-фотокнига 20*20 см (10 разворотов) с результатами съемки</p>
-            </div>
-        </div>
+               
     </div>
 </body>
 <script src="script/menu.js"></script>	
