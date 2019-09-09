17b5d300f7260c864a3d703dfaa4fc762197a87b
Nemanov Konstantin <nemanov.konstantin@yandex.ru>
2019-09-09 15:17:10 +0300

    Added rotate 180

--- prices.php
+++ prices.php
@@ -21,41 +21,43 @@
             <p>Съёмка на улице</p>
             <p>15000 ₽</p>
         </div>
-        <div class="description">
-            <div class="description-front">    
-                <div class="description-top">
-                    <h2 class="prices">ЧТО ВКЛЮЧЕНО?</h2>
-                    <p>До <b>двух часов</b> съёмки</p>
-                    <p><b>20</b> фотографий в ретуши</p>
-                    <p><b>Все остальные</b> фотографии в базовой цветокоррекции</p>
-                    <p><b>600 Р</b> за дополнительную фотографию в ретуши</p>
-                    <p>Фотографии передаются <b>на диске</b></p>
+        <div class="description-container">    
+        <div class="description-inner">
+                <div class="description-front">    
+                    <div class="description-top">
+                        <h2 class="prices">ЧТО ВКЛЮЧЕНО?</h2>
+                        <p>До <b>двух часов</b> съёмки</p>
+                        <p><b>20</b> фотографий в ретуши</p>
+                        <p><b>Все остальные</b> фотографии в базовой цветокоррекции</p>
+                        <p><b>600 Р</b> за дополнительную фотографию в ретуши</p>
+                        <p>Фотографии передаются <b>на диске</b></p>
+                    </div>
+                    <div class="description-bottom">
+                        <p><i>*Визажист, парикмахер, костюмы и прочее в стоимость услуги не входят, и, при желании клиента, оплачиваются отдельно. Призводится предоплата в резмере 20%, оставшаяся часть оплачивается в день съемки. Фотографии отдаются на диске в течение 1,5-3 месяцев после съемки.  Ваше согласие на съемку означает и согласие на то, 
+                                что отдельные работы со съемки я могу использовать в своем портфолио на различных ресурсах. </i></p>
+                        <div class="button" onclick="document.querySelector('.description-inner').classList.add('flipped')">ЗАБРОНИРОВАТЬ</div>
+                    </div>
                 </div>
-                <div class="description-bottom">
-                    <p><i>*Визажист, парикмахер, костюмы и прочее в стоимость услуги не входят, и, при желании клиента, оплачиваются отдельно. Призводится предоплата в резмере 20%, оставшаяся часть оплачивается в день съемки. Фотографии отдаются на диске в течение 1,5-3 месяцев после съемки.  Ваше согласие на съемку означает и согласие на то, 
-                            что отдельные работы со съемки я могу использовать в своем портфолио на различных ресурсах. </i></p>
-                    <div class="button">ЗАБРОНИРОВАТЬ</div>
-                </div>
+                <div class="description-back">
+                    <form action="" method="post">
+                        <h2 class="prices">ЗАБРОНИРОВАТЬ</h2>
+                        <input type="checkbox" name="studio" id="studio">
+                        <label for="studio">Аренда студии</label>
+                        <input type="text" name="name" id="name" class="text-form">
+                        <label for="name">Как вас зовут?</label>
+                        <input type="text" name="phone" id="phone" class="text-form">
+                        <label for="phone">Ваш номер телефона?</label>
+                        <input type="text" name="date" id="date" class="text-form">
+                        <label for="date">Дата съемки</label>
+                        <textarea name="message" id="" cols="30" rows="10" class="text-area-form"></textarea>
+                        <label for="message"></label>
+                        <input type="submit" value="" class="submit-form">
+                    </form>
+                </div>    
             </div>
-            <div class="description-back">
-                <form action="" method="post">
-                    <h2 class="prices">ЗАБРОНИРОВАТЬ</h2>
-                    <input type="checkbox" name="studio" id="studio">
-                    <label for="studio">Аренда студии</label>
-                    <input type="text" name="name" id="name" class="text-form">
-                    <label for="name">Как вас зовут?</label>
-                    <input type="text" name="phone" id="phone" class="text-form">
-                    <label for="phone">Ваш номер телефона?</label>
-                    <input type="text" name="date" id="date" class="text-form">
-                    <label for="date">Дата съемки</label>
-                    <textarea name="message" id="" cols="30" rows="10" class="text-area-form"></textarea>
-                    <label for="message"></label>
-                    <input type="submit" value="" class="submit-form">
-                </form>
-            </div>    
         </div>
     </div>
 </body>
 <script src="script/menu.js"></script>
-<script src='script/form.js'></script>	
+
 </html>
